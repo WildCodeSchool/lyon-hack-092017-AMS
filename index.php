f624a5bb508cac40b120b78a4ac3393b90446718
Manuel Briand <manuel.briand354@gmail.com>
2017-10-12 20:04:06 +0200

    add snippet

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $user = "ManuelBriand";
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://api.github.com/users/' . $user, [
-    'auth' => ['al2b', 'yxvd3NPC'],
+    'auth' => ['Syneot', 'BoumBoum2014'],
 ]);
 //echo $res->getStatusCode();
 // "200"
@@ -17,13 +17,68 @@ $res = $client->request('GET', 'https://api.github.com/users/' . $user, [
 //    echo $name . ': ' . implode(', ', $values) . "\r\n";
 //}
 
-//$newuser = json_decode($res->getBody());
+$newuser = json_decode($res->getBody());
 //var_dump($newuser);
-//echo $newuser->avatar_url;
+
 
 $res2 = $client->request('GET', 'https://api.github.com/users/' . $user . "/repos", [
-    'auth' => ['al2b', 'yxvd3NPC'],
+    'auth' => ['Syneot', 'BoumBoum2014'],
 ]);
 
 $newdepos = json_decode($res2->getBody());
-var_dump($newdepos);
+//var_dump($newdepos);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+</head>
+<body>
+
+<div class="container">
+<div class="row">
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+                <img src="<?php echo $newuser->avatar_url;?>">
+                <span class="card-title"></span>
+            </div>
+            <div class="card-content">
+                <h3><?php echo $newuser->login?></h3>
+            </div>
+            <ul>
+                <a href=""></a>
+            <?php
+
+            for($i = 0; $i <= 2; $i++){
+                echo "<li>" . "<a href=\"";
+                echo $newdepos[$i]->html_url;
+                echo "\">";
+                echo $newdepos[$i]->name . "</a>";
+                echo "</li>";
+            }
+            ?>
+            </ul>
+
+
+            <div class="card-action">
+                <a href="<?php echo $newuser->html_url;?>">Go in my github</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
+</html>
