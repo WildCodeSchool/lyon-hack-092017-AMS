203ac439f094205bb64fee30fdb72be7a7b7ecca
al2b <deboissieu.al@gmail.com>
2017-10-12 18:26:04 +0200

    first work with display of users info

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
 require 'vendor/autoload.php';
 
-
+$user = "ManuelBriand";
 $client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'https://api.github.com/users/al2b');
-echo $res->getStatusCode();
+$res = $client->request('GET', 'https://api.github.com/users/' . $user, [
+    'auth' => ['al2b', 'yxvd3NPC'],
+]);
+//echo $res->getStatusCode();
 // "200"
-echo $res->getHeader('content-type');
+//echo $res->getHeader('content-type');
 // 'application/json; charset=utf8'
-echo $res->getBody();
+//echo $res->getBody();
 // {"type":"User"...'
 
-foreach ($res->getBody() as $name => $values) {
-    echo $name . ': ' . implode(', ', $values) . "\r\n";
-}
+//foreach ($res->getBody() as $name => $values) {
+//    echo $name . ': ' . implode(', ', $values) . "\r\n";
+//}
 
+//$newuser = json_decode($res->getBody());
+//var_dump($newuser);
+//echo $newuser->avatar_url;
+
+$res2 = $client->request('GET', 'https://api.github.com/users/' . $user . "/repos", [
+    'auth' => ['al2b', 'yxvd3NPC'],
+]);
+
+$newdepos = json_decode($res2->getBody());
+var_dump($newdepos);
